e96df6daadc1af029a9dd9a78a70c02aae671a6b
Bhavna Shende <bhavanashende661@gmail.com>
2021-10-23 22:59:45 +0530

    Address Book

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.ArrayList;
+
+    public class AddressBook {
+
+        ArrayList<ContactsInAddressBook> contactList = new ArrayList<ContactsInAddressBook>();
+    }
+
